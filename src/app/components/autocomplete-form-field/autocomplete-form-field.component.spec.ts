d51ccbf1facf564c1dafa858dc9453730985be7a
Paweł Korobczyński <pawel.korobczynski@gmail.com>
2021-10-26 15:46:08 +0200

    add test for autocomplete-for-field component, image-container component

--- src/app/components/autocomplete-form-field/autocomplete-form-field.component.spec.ts
+++ src/app/components/autocomplete-form-field/autocomplete-form-field.component.spec.ts
@@ -1,25 +1,135 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from "@angular/common/http";
+import { DebugElement } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { By } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TranslateModule } from "@ngx-translate/core";
+import { of } from "rxjs";
+import { SUCCESS } from "src/app/data/variables-messages.data";
+import { ApiService } from "src/app/services/api.service";
+import { AutocompleteFormFieldComponent } from "./autocomplete-form-field.component";
 
-import { AutocompleteFormFieldComponent } from './autocomplete-form-field.component';
 
-describe('AutocompleteFormFieldComponent', () => {
+describe("AutocompleteFormFieldComponent", () => {
   let component: AutocompleteFormFieldComponent;
   let fixture: ComponentFixture<AutocompleteFormFieldComponent>;
+  let debug: DebugElement;
+  let apiService: ApiService;
+  const apiResponseOptions: any = { status: SUCCESS, data: ["option1", "option2", "option3"] };
 
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     declarations: [ AutocompleteFormFieldComponent ]
-  //   })
-  //   .compileComponents();
-  // });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AutocompleteFormFieldComponent],
+      imports: [
+        HttpClientModule,
+        MatDialogModule,
+        MatAutocompleteModule,
+        MatFormFieldModule,
+        FormsModule,
+        MatInputModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        { provide: ApiService }
+      ]
+    })
+    .compileComponents();
+  });
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(AutocompleteFormFieldComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AutocompleteFormFieldComponent);
+    component = fixture.componentInstance;
+    debug = fixture.debugElement;
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+    apiService = TestBed.inject(ApiService);
+    spyOn(apiService, "getAutocompleteOptions").and.returnValue(of(apiResponseOptions));
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should set form control from input", async () => {
+    const newFormControl: FormControl = new FormControl("newFormControl");
+    const input: DebugElement = debug.query(By.css("input"));
+    component.formFieldControl = newFormControl;
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(input.nativeElement.value).toEqual("newFormControl");
+  });
+
+  it("should get autocomplete options from api for given property", () => {
+    component.acProperty = "customProperty";
+    fixture.detectChanges();
+    expect(apiService.getAutocompleteOptions).toHaveBeenCalledWith("customProperty");
+    expect(component.acOptions.length).toEqual(apiResponseOptions.data.length);
+  });
+
+  it("should generate autocomplete option elements", () => {
+    component.acProperty = "customProperty";
+    fixture.detectChanges();
+
+    const input: DebugElement = debug.query(By.css("input"));
+    input.nativeElement.dispatchEvent(new Event("focusin"));
+    fixture.detectChanges();
+
+    const autocompleteOptions: DebugElement[] = debug.queryAll(By.css("mat-option"));
+    expect(autocompleteOptions.length).toEqual(apiResponseOptions.data.length);
+  });
+
+  it("should filter options when user type data into input", () => {
+    component.acProperty = "customProperty";
+    component.formFieldControl = new FormControl("");
+    fixture.detectChanges();
+    component.formFieldControl.setValue("1");
+    fixture.detectChanges();
+    expect(component.filteredAcOptions.length).toEqual(1);
+  });
+
+  it("should generate only filtered option elements", () => {
+    component.acProperty = "customProperty";
+    component.formFieldControl = new FormControl("");
+    fixture.detectChanges();
+    component.formFieldControl.setValue("1");
+    fixture.detectChanges();
+
+    const input: DebugElement = debug.query(By.css("input"));
+    input.nativeElement.dispatchEvent(new Event("focusin"));
+    fixture.detectChanges();
+
+    const autocompleteOptions: DebugElement[] = debug.queryAll(By.css("mat-option"));
+    expect(autocompleteOptions.length).toEqual(1);
+  });
+
+  it("should hide clear icon when input is empty", () => {
+    fixture.detectChanges();
+    const matIcon: DebugElement = debug.query(By.css("mat-icon"));
+    expect(component.formFieldControl.value).toEqual("")
+    expect(matIcon.nativeElement.classList).toContain("hideCloseIcon");
+  });
+
+  it("should show clear icon when input has value", () => {
+    component.formFieldControl.setValue("customValue");
+    fixture.detectChanges();
+    const matIcon: DebugElement = debug.query(By.css("mat-icon"));
+    expect(matIcon.nativeElement.classList).not.toContain("hideCloseIcon");
+  });
+  
+  it("should clear form control when user click remove icon", () => {
+    spyOn(component, "clearFormFieldHandler").and.callThrough();
+    fixture.detectChanges();
+    const clearIcon: DebugElement = debug.query(By.css("mat-icon"));
+    component.formFieldControl.setValue("customValue");
+    clearIcon.nativeElement.dispatchEvent(new Event("click"));
+    fixture.detectChanges();
+    expect(component.formFieldControl.value).toEqual("");
+    expect(component.clearFormFieldHandler).toHaveBeenCalled();
+  });
 });
--- src/app/components/image-container/image-container.component.spec.ts
+++ src/app/components/image-container/image-container.component.spec.ts
@@ -1,25 +1,91 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ImageContainerComponent } from "./image-container.component";
+import { ApiService } from "../../services/api.service";
+import { DebugElement } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { By } from "@angular/platform-browser";
+import { NO_IMAGE_URL } from "src/app/data/variables-messages.data";
 
-import { ImageContainerComponent } from './image-container.component';
 
-describe('ImageContainerComponent', () => {
+describe("ImageContainerComponent", () => {
   let component: ImageContainerComponent;
   let fixture: ComponentFixture<ImageContainerComponent>;
+  let debug: DebugElement;
+  let apiService: ApiService;
+  const URL_FROM_API: string = "urlFromApi";
 
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     declarations: [ ImageContainerComponent ]
-  //   })
-  //   .compileComponents();
-  // });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ImageContainerComponent],
+      providers: [ApiService],
+      imports: [HttpClientModule, MatDialogModule, MatProgressSpinnerModule]
+    })
+    .compileComponents();
+  });
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(ImageContainerComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ImageContainerComponent);
+    component = fixture.componentInstance;
+    debug = fixture.debugElement;
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+    apiService = debug.injector.get(ApiService);
+    spyOn(apiService, "getImageApiUrl").and.returnValue(URL_FROM_API);
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should show spinner when loading image and hide img element", () => {
+    const matSpinner: DebugElement = debug.query(By.css("mat-spinner"));
+    const img: DebugElement = debug.query(By.css("img"));
+
+    expect(component.imageLoading).toBeTruthy();
+    expect(matSpinner).toBeDefined();
+    expect(img.nativeElement.classList).toContain("hideImage");
+  });
+
+  it("should hide spinner when image loaded and show img element", () => {
+    const img: DebugElement = debug.query(By.css("img"));
+    img.nativeElement.dispatchEvent(new Event("load"));
+    fixture.detectChanges();
+
+    const matSpinner: DebugElement = debug.query(By.css("mat-spinner"));
+    expect(component.imageLoading).toBeFalsy();
+    expect(matSpinner).toBeNull();
+    expect(img.nativeElement.classList).not.toContain("hideImage");
+  });
+
+  it("should show default image when loading image failed", () => {
+    spyOn(component, "errorHandler").and.callThrough();
+    const img: DebugElement = debug.query(By.css("img"));
+    img.nativeElement.dispatchEvent(new Event("error"));
+    fixture.detectChanges();
+
+    expect(component.errorHandler).toHaveBeenCalled();
+    expect(img.nativeElement.attributes.getNamedItem("src").value).toEqual(NO_IMAGE_URL);
+  });
+
+  it("should show background on default", () => {
+    const background: DebugElement = debug.query(By.css(".backgroundImage"));
+    expect(background).toBeDefined();
+  });
+
+  it("should hide background when not needed", () => {
+    component.showBackgroundImage = false;
+    fixture.detectChanges();
+    const background: DebugElement = debug.query(By.css(".backgroundImage"));
+    expect(background).toBeNull();
+  });
+
+  it("should get image url from api service", () => {
+    const img: DebugElement = debug.query(By.css("img"));
+    component.imageUrlId = "customImageUrlId";
+    fixture.detectChanges();
+    expect(apiService.getImageApiUrl).toHaveBeenCalled();
+    expect(img.nativeElement.attributes.getNamedItem("src").value).toEqual(URL_FROM_API);
+  });
 });
